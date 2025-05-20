d01bf87965f7c1600eed394c96a1fb33fef18c31
afassfox <afassfox@gmail.com>
2025-05-20 13:49:32 +0300

    fixed Motivation section responsiveness

--- src/components/sections/Motivation.tsx
+++ src/components/sections/Motivation.tsx
@@ -8,7 +8,7 @@ export default function Motivation() {
           <div className='absolute top-0 left-0 w-20 h-20 sm:w-30 sm:h-30 bg-body z-10 blur-2xl sm:blur-3xl opacity-70'></div>
           <div className='absolute top-0 right-0 w-20 h-20 sm:w-30 sm:h-30 bg-body z-10 blur-2xl sm:blur-3xl opacity-70'></div>
 
-          <ul className='flex flex-col text-center gap-4 sm:gap-6 md:gap-8 py-10 sm:py-15 md:py-20'>
+          <ul className='flex flex-col lg:flex-row text-center gap-8 sm:gap-10 md:gap-15 lg:gap-12 py-10 sm:py-15 md:py-20'>
             <li className='font-semibold text-4xl sm:text-6xl md:text-8xl text-[#f1e9fe] transition-all duration-300'>
               تقدَّم
             </li>
