c40c2015334f85464ac1047cda17e29476a829b2
afassfox <afassfox@gmail.com>
2025-05-20 13:40:17 +0300

    fixed Motivation section responsiveness

--- src/components/sections/Motivation.tsx
+++ src/components/sections/Motivation.tsx
@@ -8,16 +8,16 @@ export default function Motivation() {
           <div className='absolute top-0 left-0 w-30 h-30 bg-body z-10 blur-3xl opacity-70'></div>
           <div className='absolute top-0 -right-10 w-30 h-30 bg-body z-10 blur-3xl opacity-70'></div>
           <ul className='flex flex-row justify-center gap-22'>
-            <li className='font-semibold md:text-8xl text-4xl text-[#f1e9fe]'>
+            <li className='font-semibold md:text-8xl sm:text-6xl text-4xl text-[#f1e9fe]'>
               تقدَّم
             </li>
-            <li className='text-heading-1 font-semibold md:text-8xl text-4xl'>
+            <li className='text-heading-1 font-semibold md:text-8xl sm:text-6xl text-4xl'>
               ازدهر
             </li>
-            <li className='font-semibold md:text-8xl text-4xl text-[#f1e9fe]'>
+            <li className='font-semibold md:text-8xl sm:text-6xl text-4xl text-[#f1e9fe]'>
               تطوّر
             </li>
-            <li className='text-heading-1 font-semibold md:text-8xl text-4xl'>
+            <li className='text-heading-1 font-semibold md:text-8xl sm:text-6xl text-4xl'>
               إنطلق
             </li>
           </ul>
